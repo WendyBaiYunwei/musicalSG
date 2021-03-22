e10ac7aa08f12e0f4ca80df2f387b4a7769fb3cd
whitesnowhahorha <yunweibai@gmail.com>
2021-03-22 13:54:42 +0800

    fill in a model for train

--- train.py
+++ train.py
@@ -1,26 +1,62 @@
-from sklearn.ensemble import RandomForestClassifier
-from sklearn import metrics
+from keras import backend as K
+from keras_preprocessing import sequence
+from keras.layers import *
+from keras.models import Model
 import numpy as np
 import time
 import os
-
+from sklearn.metrics import accuracy_score
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.datasets import make_classification
 
 def train():
-    # zero padding
-    input_vec = sequence.pad_sequences(np.load('dataset/input_vector.npy', allow_pickle=True))
-    target_vec = np.load('dataset/target_vector.npy')
+    train = np.load('dataset/train.npy', allow_pickle=True)
 
-    input_dim = input_vec.shape[2]
-    output_dim = target_vec.shape[1]
+    x = np.zeros((1801 * 185, 32))
+    y = np.zeros((1801 * 185))
+    n = 0
+    for i in range(1801):
+        # pad the current song's bars
+        for j in range(185):
+            if j < len(train[i][1]) and 1 in train[i][1][j]:
+                y[n] = train[i][1][j].index(1)
+            else:
+                y[n] = 24
+            
+            for k in range(32):
+                if j < len(train[i][0]) and k < len(train[i][0][j]) and 1 in train[i][0][j][k]:
+                    x[n][k] = train[i][0][j][k].index(1)
+                else:
+                    x[n][k] = 12
+            n += 1
 
-    clf=RandomForestClassifier(n_estimators=100)
+    train = np.load('dataset/test.npy', allow_pickle=True)
 
-    clf.fit(input_vec,target_vec)
+    test_x = np.zeros((train.shape[0] * 185, 32))
+    test_y = np.zeros((train.shape[0] * 185))
 
-    print("Done training")
+    n = 0
+    for i in range(train.shape[0]):
+        for j in range(185):
+            if j < len(train[i][1]) and 1 in train[i][1][j]:
+                test_y[n] = train[i][1][j].index(1)
+            else:
+                test_y[n] = 24
+            
+            for k in range(32):
+                if j < len(train[i][0]) and k < len(train[i][0][j]) and 1 in train[i][0][j][k]:
+                    test_x[n][k] = train[i][0][j][k].index(1)
+                else:
+                    test_x[n][k] = 12
+            n += 1
 
-    # y_pred=clf.predict(X_test)
-    # print("Accuracy:",metrics.accuracy_score(y_test, y_pred))
+    clf = DecisionTreeClassifier()
+    clf = clf.fit(x, y)
+    y_pred = clf.predict(test_x)
+
+    print("Done!")
+
+    print(accuracy_score(test_y, y_pred))
 
 if __name__ == '__main__':
     train()
